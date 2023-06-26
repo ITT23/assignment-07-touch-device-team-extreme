faaae10c7744eb3d72620317b5648e798f743989
tina-e <tina.emmert@web.de>
2023-06-26 13:23:51 +0200

    add dippid sender class

---
+++ event_streamer.py
@@ -0,0 +1,39 @@
+import socket
+
+
+class EventStreamer:
+    def __init__(self, ip: str, width: int, height: int) -> None:
+        self.IP = ip
+        self.PORT = 5700
+        self.width = width
+        self.height = height
+        self.dippid_data = dict()
+        self.events = dict()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def add_to_stream(self, type: str, x: float, y: float):
+        """
+        add event to stream which can be sent later
+        """
+        if type != 'touch' and type != 'hover':
+            return
+        index = len(self.events)
+        current_event = dict()
+        current_event['type'] = type
+        current_event['x'] = int(x / self.width)
+        current_event['y'] = int(y / self.height)
+        self.events[index] = current_event
+        self.dippid_data['events'] = self.events
+        print(current_event)
+
+
+    def send_stream(self):
+        """
+        call this function everytime you want to send data
+        (for example in a certain intervall)
+        """ 
+        self.sock.sendto(self.dippid_data.encode(), (self.IP, self.PORT))
+        self.dippid_data = dict()
+        self.events = dict()
+        print("-------- event stream sent --------")
+
